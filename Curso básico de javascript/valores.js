21d9cff3b11ee086f9ce96e75c4a27fe1b3e8fd7
luisdue3 <luisdue3@gmail.com>
2023-02-28 16:25:45 -0500

    Initial commit

---
+++ Curso básico de javascript/valores.js
@@ -0,0 +1,18 @@
+var nombre = "Oscar";
+
+// declarar 
+var edad;
+// inicializar
+
+edad=30;
+
+edad
+
+var elementos=["Computadora","Celular"];
+
+var persona={
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
