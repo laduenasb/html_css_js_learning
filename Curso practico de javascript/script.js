89ed2ec080387bfe74e60f6da94f2d5ed6490a1e
luisdue3 <luisdue3@gmail.com>
2023-03-01 12:59:46 -0500

    Listening to button btnCalcular

--- Curso practico de javascript/script.js
+++ Curso practico de javascript/script.js
@@ -4,6 +4,8 @@ const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult=document.querySelector("#result");
 
+btn.addEventListener("click", btnOnClick);
+
 function btnOnClick() {
     const sumaInputs = Number(input1.value)+Number(input2.value);
     pResult.innerText = "Resultado: "+ sumaInputs;
