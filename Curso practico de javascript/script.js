e63a410e8950f2472cb9c4781247549536052290
luisdue3 <luisdue3@gmail.com>
2023-03-01 11:18:53 -0500

    Add onclick to button and execute btnOnClick function

--- Curso practico de javascript/script.js
+++ Curso practico de javascript/script.js
@@ -1,28 +1,10 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.getElementById("pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult=document.querySelector("#result");
 
-console.log(h1);
-
-console.log({
-    h1,p,parrafito,pid,input
-});
-
-h1.innerHTML = "Patito <br> Feo";
-h1.innerText = "Patito <br> Feo";
-// console.log(h1.getAttribute("class"));
-// h1.setAttribute("class","rojo");
-
-h1.classList.add("rojo");
-h1.classList.remove("verde");
-// h1.classList.toggle("verde");
-// h1.classList.contains("verde");
-input.value="456";
-
-const img = document.createElement("img");
-img.setAttribute("src","https://images.pexels.com/photos/11662238/pexels-photo-11662238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-console.log(img);
-pid.innerHTML="";
-pid.append(img);
+function btnOnClick() {
+    const sumaInputs = Number(input1.value)+Number(input2.value);
+    pResult.innerText = "Resultado: "+ sumaInputs;
+}
