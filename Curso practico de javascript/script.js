d4cb913cff6cf0e063c73a6ffc35767f9f7340e6
luisdue3 <luisdue3@gmail.com>
2023-03-01 11:03:59 -0500

    add html code from js

--- Curso practico de javascript/script.js
+++ Curso practico de javascript/script.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
 const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
+const pid = document.getElementById("pid");
 const input = document.querySelector("input");
 
 console.log(h1);
@@ -9,3 +9,20 @@ console.log(h1);
 console.log({
     h1,p,parrafito,pid,input
 });
+
+h1.innerHTML = "Patito <br> Feo";
+h1.innerText = "Patito <br> Feo";
+// console.log(h1.getAttribute("class"));
+// h1.setAttribute("class","rojo");
+
+h1.classList.add("rojo");
+h1.classList.remove("verde");
+// h1.classList.toggle("verde");
+// h1.classList.contains("verde");
+input.value="456";
+
+const img = document.createElement("img");
+img.setAttribute("src","https://images.pexels.com/photos/11662238/pexels-photo-11662238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+console.log(img);
+pid.innerHTML="";
+pid.append(img);
