6838e9068401dbfb031b25112f02c41a39a5fcd8
luisdue3 <luisdue3@gmail.com>
2023-03-01 13:18:33 -0500

    add event to form and execute botonSumar

--- Curso practico de javascript/script.js
+++ Curso practico de javascript/script.js
@@ -1,12 +1,24 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult=document.querySelector("#result");
 
-btn.addEventListener("click", btnOnClick);
+// form.addEventListener("submit", botonSumar);
 
-function btnOnClick() {
+// function botonSumar(event) {
+//     // console.log(event);
+//     event.preventDefault();
+//     const sumaInputs = Number(input1.value)+Number(input2.value);
+//     pResult.innerText = "Resultado: "+ sumaInputs;
+// }
+
+btn.addEventListener("click", botonSumar);
+
+function botonSumar(event) {
+    // console.log(event);
+    // event.preventDefault();
     const sumaInputs = Number(input1.value)+Number(input2.value);
     pResult.innerText = "Resultado: "+ sumaInputs;
 }
